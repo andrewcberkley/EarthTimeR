abeceb38dc2e157df803c772f6eac54988499c05
berks <andrewcberkley@gmail.com>
2021-10-12 13:21:31 +0200

    Initial commit

---
+++ R/choropleth_function.R
@@ -0,0 +1,19 @@
+#' A Choropleth Function
+#'
+#' This function allows you to format your long dataframe for an EarthTime choropleth.
+#' @param new_df_name Set the name of the new dataframe..
+#' @param long_df Specify the name of the long dataframe you wish to format..
+#' @param date Specify the name of the date column.
+#' @param value Specify the name of the value you'd like to spread across the wide dataframe..
+#' @keywords choropleth
+#' @export
+#' @examples
+#' choropleth()
+
+choropleth <- function(new_df_name, long_df, date, value){
+  new_df_name <- tidyverse::long_df %>%
+    group_by(date) %>%
+    mutate(idx = row_number()) %>%
+    spread(date, value) %>%
+    select(-idx)
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# EarthTimeR
+ EarthTimeR is a package created to make data wrangling easier before ingesting data into EarthTime, which is a collaboration between Carnegie Mellon University's CREATE Lab and the World Economic Forum allowing individuals to examine major environmental, social, and political phenomenon across time, across space, and between each other.
