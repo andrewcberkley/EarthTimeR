4a001f4981b7fb6da31ec8d1e40c15b0260b0471
berks <andrewcberkley@gmail.com>
2021-10-14 14:28:31 +0200

    Create append_new_data_layer_to_CSV_layers_sheet.R

---
+++ R/append_new_data_layer_to_CSV_layers_sheet.R
@@ -0,0 +1,15 @@
+#' An Append New Data Layer to CSV Layers Sheet Function
+#'
+#' This function allows for the appending of a new data layer into your CSV layers sheet. Can be used in conjunction with map functions such initial_ingestion() or overwite_sheet().
+
+#' @param csv_layer Specify the name of the csv layer output from previous functions.
+#' @param ss Something that identifies a Google Sheet: its file ID, a URL from which we can recover the ID, an instance of googlesheets4_spreadsheet (returned by sheets_get()), or a dribble, which is how googledrive represents Drive files. Processed through as_sheets_id().
+#' @param sheet Sheet to append to, in the sense of "worksheet" or "tab". You can identify a sheet by name, with a string, or by position, with a number.
+#' @keywords append CSV layers
+#' @export
+#' @examples
+#' append_new_data_layer_to_CSV_layers()
+
+append_new_data_layer_to_CSV_layers  <- function(csv_layer, ss, sheet = "CSV Layers"){
+  googlesheets4::sheets_append(data = csv_layer, sheets_get(ss), sheet = "CSV Layers")
+}
