f71f848bb4adadd8da371bebad3b98b90d218e1e
berks <andrewcberkley@gmail.com>
2021-10-21 16:09:55 +0200

    Update create_dot_map_function.R

--- R/create_dot_map_function.R
+++ R/create_dot_map_function.R
@@ -1,10 +1,15 @@
 #' A Dot Map Creation Function
 #'
-#' This function splits a dotmap from a dataframe. It will output a .bin file in your working directory for you to later updload to your specific EarthTime instance.
+#' This function splits a dot map from a dataframe. It will output a .bin file in your working directory for you to later upload to your specific EarthTime instance.
 
-#' @param dataframe Specify the name of the wide dataframe that should be split
-#' @param categories_column Specify the column name (in quotes) or the column number that contains the various categories.
-#' @keywords split categories
+#' @param dataframe Specify the name of the dataframe that you'd like to transform into a dot map .bin file.
+#' @param latitude_column Specify the column name (in quotes) or the column number that contains the latitudinal coordinates.
+#' @param longitude_column Specify the column name (in quotes) or the column number that contains the longitudinal coordinates.
+#' @param value_column Specify the column name (in quotes) or the column number that contains the primary value being visualized.
+#' @param date_column Specify the column name (in quotes) or the column number that contains the date category.
+#' @param date_format Specify format of the dates. The full set of format codes supported varies across platforms, because this function draws on Python it will call from the platform's C library's strftime() function, and platform variations are common. The date format codes required are the C standard (1989 version), and these work on all platforms with a standard C implementation. Note that the 1999 version of the C standard added additional format codes. The function defaults to "%Y-%m-%d".
+#' @param rgb_color_scheme Specify RGB  tuple of float values in a closed interval between 0 and 255. Defaults to "255,0,0"
+#' @keywords dot map
 #' @export
 #' @examples
 #' \dontrun{
@@ -15,7 +20,7 @@
 #py_config()
 #py_install("pandas")
 
-split_categories <-  function(dataframe, latitude_column, longitude_column, value_column, date_column, date_format, rgb_color_scheme){
+split_categories <-  function(dataframe, latitude_column, longitude_column, value_column, date_column, date_format = "%Y-%m-%d", rgb_color_scheme = "[255,0,0]"){
   
   write.csv(dataframe, paste0(dataframe, ".csv"), row.names = FALSE, na = "")
   
