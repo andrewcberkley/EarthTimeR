ae8a5d1ea51359740ca88e1fd3f6e6ae9610a984
berks <andrewcberkley@gmail.com>
2021-10-22 19:01:33 +0200

    updated python functions

--- R/zzz.R
+++ R/zzz.R
@@ -1,8 +1,12 @@
-LonLatToPixelXY <- NULL
-FormatEpoch <- NULL
-PackColor <- NULL
-hex2rgb <- NULL
-create_python_dotmap <- NULL
+.onLoad <- function(libname, pkgname) {
+  user_permission <- utils::askYesNo("Install miniconda? downloads 50MB and takes time")
+  
+  if (isTRUE(user_permission)) {
+    reticulate::install_miniconda()
+  } else {
+    message("You should run `reticulate::install_miniconda() before using this package")
+  }
+}
 
 .onLoad <- function(libname, pkgname) {
   the_module <- reticulate::import_from_path(module = "dotmap_via_reticulate", path = system.file("python", package = packageName()))
