7711ad77f8617e662bf68e0f9f066e4b789bf9f6
berks <andrewcberkley@gmail.com>
2021-10-22 19:39:59 +0200

    updated python functions

--- R/zzz.R
+++ R/zzz.R
@@ -15,10 +15,6 @@ hex2rgb <- NULL
 create_python_dotmap <- NULL
 
 .onLoad <- function(libname, pkgname) {
-  the_module <- reticulate::import_from_path(module = "dotmap_via_reticulate", path = system.file("python", package = packageName()))
-  LonLatToPixelXY <<- the_module$LonLatToPixelXY
-  FormatEpoch <<- the_module$FormatEpoch
-  PackColor <<- the_module$PackColor
-  hex2rgb <<- the_module$hex2rgb
-  create_python_dotmap <<- the_module$create_python_dotmap
+  the_module <- reticulate::import(
+    module = "dotmap_via_reticulate", delay_load = TRUE)
 }
