d13b6804714eab8e994ae3da9e20b617a9680030
berks <andrewcberkley@gmail.com>
2021-10-25 13:52:30 +0200

    updated flow map functions

--- R/create_flowMap_function.R
+++ R/create_flowMap_function.R
@@ -8,15 +8,13 @@
 #' @param centroids_json Defaults to "centroids_by_iso_alpha_3.json" included with the package.
 #' @param value_column Specify the column name (in quotes) that contains the primary value being visualized.
 #' @param date_column Specify the column name (in quotes) that contains the date category.
-#' @param date_format Specify format of the dates. The full set of format codes supported varies across platforms, because this function draws on Python it will call from the platform's C library's strftime() function, and platform variations are common. The date format codes required are the C standard (1989 version), and these work on all platforms with a standard C implementation. Note that the 1999 version of the C standard added additional format codes. The function defaults to "%Y-%m-%d".
-#' @param rgb_color_scheme Specify RGB  tuple of float values in a closed interval between 0 and 255. Defaults to "255,0,0" (i.e. - the color red).
 #' @param projected_coordinate_system A flat, two-dimensional representation of the Earth based on a sphere or spheroid geographic coordinate system, but it uses linear units of measure for coordinates, so that calculations of distance and area are easily done in terms of those same units. Defaults to EPSG:3857 (WGS 84 / Pseudo-Mercator) used for rendering maps in Spherical Mercator, Google Maps, OpenStreetMap, Bing, ArcGIS, ESRI, etc.
 #' @param output_dir Output director for .bin file. Defaults to current working directory.
 #' @keywords flow map
 #' @export
 #' @examples
 #' \dontrun{
-#' create_dotMap <-  function(dataframe, latitude_column, longitude_column, value_column, date_column)
+#' create_flowMap <-  function(dataframe, origin, destination, value_column, date_column)
 #'}
 
 #suppressWarnings(use_python("C:/ProgramData/Anaconda3/", required = TRUE))
