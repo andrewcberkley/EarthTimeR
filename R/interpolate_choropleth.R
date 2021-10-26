22c94b3dbb23c3c2f298765d8b31519a4ea9b1ef
berks <andrewcberkley@gmail.com>
2021-10-26 17:34:54 +0200

    Update interpolate_choropleth.R

--- R/interpolate_choropleth.R
+++ R/interpolate_choropleth.R
@@ -1,67 +1,71 @@
 #' A Choropleth Interpolation Function
 #'
-#' While most interpolation functions are performed on "long" dataframes, this function performs linear interpolation of missing values on a choropleth style dataframe or any type of "wide" dataframe.
+#' While most interpolation functions are performed on "long" dataframes, this function performs interpolation of missing values on a choropleth style dataframe or any type of "wide" dataframe.
 
 #' @param dataframe Specify the name of the dataframe that you'd like to transform into a dot map .json file.
-#' @param start_year  Specify the year the data begins. Due to the nature of the function, it is highly recommended that that date intervals are in years rather than days or months..
+#' @param start_year  Specify the year the data begins. Due to the nature of the function, it is highly recommended that that date intervals are in years rather than days or months.
 #' @param end_year Specify the year the data ends. Due to the nature of the function, it is highly recommended that that date intervals are in years rather than days or months.
+#' @param time_intervals A vector of all the dataframe's time intervals that will be used as reference points for the interpolation function. Example: c(2002, 2007, 2011, 2013, 2019)
+#' @param interpolation_type The level of interpolation for the data. Defaults to "linear"..
 #' @keywords linear interpolation
 #' @export
 #' @examples
 #' \dontrun{
-#' new_df <- interpolate_choropleth(dataframe, 1)
+#' interpolated_df <- interpolate_choropleth(dataframe, start_year, end_year, time_intervals, interpolation_type)
 #'}
-#'
 
+interpolate_choropleth <- function(dataframe, start_year, end_year, time_intervals, interpolation_type = "linear"){
 
-
-choropleth_linear_interpolate0 <- function(y1, y2, side) {
-if (side==0) {return(y1)}
-else {return(y2)}
-}
-
-choropleth_linear_interpolate1 <- function(x1, x2, y1, y2, val) {
-  slope <- (y2-y1)/(x2-x1)
-  return(y1+(val-x1)*slope)
-}
-
-c_names <- c("key_name", start_year:end_year)
-
-list_edges <- rbind(
-  cbind(2002, 2007),
-  cbind(2007, 2011),
-  cbind(2011, 2013),
-  cbind(2013, 2019))
-
-interpolate_choropleth <- function(my.df, type) {
-  
-  k <- 2
-  
-  for (i in 1:nrow(list_edges)) {
-    
-    x1 <- list_edges[i,1]
-    x2 <- list_edges[i,2]
-    
-    for (j in 1:(x2-x1-1)) {
-      
-      my.df <- cbind(my.df, apply(
-        my.df, 1, 
-        function(x) {
-          y1 <- as.numeric(x[k])
-          y2 <- as.numeric(x[k+1])
-          if (type==0) {
-            if (is.na(y1) | is.na(y2)) {return(NA)}
-            else {return(choropleth_linear_interpolate0(y1, y2, 0))}
-          } else if (type==1) {
-            if (is.na(y1) | is.na(y2)) {return(NA)}
-            else {return(choropleth_linear_interpolate1(x1, x2, y1, y2, x1+j))}
-          }
-        }))
-      colnames(my.df)[ncol(my.df)] <- as.character(x1+j)
-    }
-    
-    k <- k+1
+  choropleth_linear_interpolate0 <- function(y1, y2, side) {
+  if (side==0) {return(y1)}
+  else {return(y2)}
   }
   
-  return(my.df)
+  choropleth_linear_interpolate1 <- function(x1, x2, y1, y2, val) {
+    slope <- (y2-y1)/(x2-x1)
+    return(y1+(val-x1)*slope)
+  }
+  
+  c_names <- c("key", start_year:end_year)
+  
+  list_edges <- rbind(
+    cbind(2002, 2007),
+    cbind(2007, 2011),
+    cbind(2011, 2013),
+    cbind(2013, 2019)
+    )
+  
+  interpolation_function <- function(dataframe, type) {
+    
+    k <- 2
+    
+    for (i in 1:nrow(list_edges)) {
+      
+      x1 <- list_edges[i,1]
+      x2 <- list_edges[i,2]
+      
+      for (j in 1:(x2-x1-1)) {
+        
+        dataframe <- cbind(dataframe, apply(
+          dataframe, 1, 
+          function(x) {
+            y1 <- as.numeric(x[k])
+            y2 <- as.numeric(x[k+1])
+            if (type==0) {
+              if (is.na(y1) | is.na(y2)) {return(NA)}
+              else {return(choropleth_linear_interpolate0(y1, y2, 0))}
+            } else if (type==1) {
+              if (is.na(y1) | is.na(y2)) {return(NA)}
+              else {return(choropleth_linear_interpolate1(x1, x2, y1, y2, x1+j))}
+            }
+          }))
+        colnames(dataframe)[ncol(dataframe)] <- as.character(x1+j)
+      }
+      
+      k <- k+1
+    }
+    
+    return(dataframe)
+  }
+
 }
