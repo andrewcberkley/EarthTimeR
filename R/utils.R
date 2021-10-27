e8a38338be3bf84f715fa6fa7d4a5520ff60cfb0
berks <andrewcberkley@gmail.com>
2021-10-27 14:02:43 +0200

    Update utils.R

--- R/utils.R
+++ R/utils.R
@@ -1,13 +1,9 @@
-#' A Utility Function
-#'
-#' Functions to help with more complex downstream functions for the EarthTimeR package.
-
-choropleth_linear_interpolate0 <- function(y1, y2, side) {
+linear_interpolate0 <- function(y1, y2, side) {
   if (side==0) {return(y1)}
   else {return(y2)}
 }
 
-choropleth_linear_interpolate1 <- function(x1, x2, y1, y2, val) {
+linear_interpolate1 <- function(x1, x2, y1, y2, val) {
   slope <- (y2-y1)/(x2-x1)
   return(y1+(val-x1)*slope)
 }
@@ -30,10 +26,10 @@ interpolation_function <- function(dataframe, type) {
           y2 <- as.numeric(x[k+1])
           if (type==0) {
             if (is.na(y1) | is.na(y2)) {return(NA)}
-            else {return(choropleth_linear_interpolate0(y1, y2, 0))}
+            else {return(linear_interpolate0(y1, y2, 0))}
           } else if (type==1) {
             if (is.na(y1) | is.na(y2)) {return(NA)}
-            else {return(choropleth_linear_interpolate1(x1, x2, y1, y2, x1+j))}
+            else {return(linear_interpolate1(x1, x2, y1, y2, x1+j))}
           }
         }))
       colnames(dataframe)[ncol(dataframe)] <- as.character(x1+j)
