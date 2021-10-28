97e6810f794d3452e811cdb65e47abced53bf637
berks <andrewcberkley@gmail.com>
2021-10-28 15:11:35 +0200

    Update longTransform.R

--- R/longTransform.R
+++ R/longTransform.R
@@ -3,8 +3,8 @@
 #' A few special map types in EarthTime (i.e. - line strings, point flows, points, and raster maps) are from "long" dataframes before using create_dotMap() and/or create_flowMap() to generate a .bin  file. This function transforms the dataframe in an appropriate format to successfully utilize the aforementioned functions.
 
 #' @param dataframe Specify the name of the dataframe that you'd like to transform.
-#' @param origin_name Specify the name (in quotes) of the origin location.
-#' @param destination_name Specify the name (in quotes) of the final destination.
+#' @param origin_name Specify the name (in quotes) of the origin location. You can always respecify this while invoking the create_flowMap() function.
+#' @param destination_name Specify the name (in quotes) of the final destination. You can always respecify this while invoking the create_flowMap() function.
 #' @param value_name
 #' @param value_columns A vector of all the dataframe's time intervals that will be used as reference points for the interpolation function. Use, e.g., value_columns = c(3:223)
 #' @keywords transform long dataframe special map
